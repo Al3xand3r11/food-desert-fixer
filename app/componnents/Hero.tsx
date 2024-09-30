9498a6c566b25df86d68f2ec2ce2c4f0ce2836e1
Al3xand3r11 <alexander.nance11@gmail.com>
2024-09-30 10:39:06 -0700

    spacing change

--- app/componnents/Hero.tsx
+++ app/componnents/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero () {
     return (
         <div className="content-center h-screen text-white">
             <div className="flex grid-cols-2 text-center">
-                <div className="w-full ml-48 mt-32">
+                <div className="w-full ml-48 mt-16">
                     <h1 className="text-left text-6xl">
                         Find and help your local food
                         desert. Make a mark in
