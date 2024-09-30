0a9c98b9a9bae7bf2863efa6cbe4b20d26606ef3
Al3xand3r11 <alexander.nance11@gmail.com>
2024-09-30 10:34:12 -0700

    Mission

--- app/componnents/Mission.tsx
+++ app/componnents/Mission.tsx
@@ -1,7 +1,11 @@
 export default function Mission () {
     return (
-        <>
-            <h1>Mission Section</h1>
-        </>
+        <div className="h-auto">
+            <div className="w-full flex  flex-col place-items-center">
+            <h1 className="text-white pb-8 text-6xl">Mission</h1>
+            <h1 className="text-white w-1/2 text-4xl text-center font-bold">Food deserts can be a silent killer in the community. When people do not have  access to fresh produce, poultry or even healthier alternatives for snacks, it can be the precursor to a litany of health ailments in the future.
+            This tool has been created to start the conversation and visualize how you can make an impact for your community.</h1>
+            </div>
+        </div>
     )
 }
