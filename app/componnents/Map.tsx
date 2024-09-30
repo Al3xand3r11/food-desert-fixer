cebd158ec7f22176cfa8111bbe04b15471657188
Al3xand3r11 <alexander.nance11@gmail.com>
2024-09-30 11:09:16 -0700

    working zipcode

--- app/componnents/Map.tsx
+++ app/componnents/Map.tsx
@@ -6,8 +6,12 @@ export default function Map () {
     return (
         <div className="h-screen items-center">
             <div className="content-center flex flex-col place-items-center w-full ">
-                <h1>Hero Section</h1>
+                <div className="pb-12 flex-col flex">
+                    <label className="text-white pb-2 text-2xl">Enter Zip Code:</label>
+                    <input type="text" id="zipcode" placeholder="ex: 90210" className="rounded-full outline-none bg-transparent px-4 text-white  border-red border-2 text-4xl py-1 placeholder-white placeholder-opacity-30"  pattern="^\d{5}(-\d{4})?$" required/>
+                </div>
                 <MapContainer/>
+                
             </div>
             
         </div>
